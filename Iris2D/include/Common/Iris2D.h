bfeef00211c82b89c3251c1afa872afdb949af91
Huisama <yuwenhuisama@outlook.com>
2018-06-26 03:44:51 +0800

    First Initialize.

---
+++ Iris2D/include/Common/Iris2D.h
@@ -0,0 +1,18 @@
+#ifndef _H_IRIS2D_
+#define _H_IRIS2D_
+
+#include "Iris2DCompileConfigure.h"
+
+#if IR_API_VERSION == 0
+#else
+#include "../DirectX/Iris2D/IrisApp.h"
+#include "../DirectX/Iris2D/IrisGraphics.h"
+#include "../DirectX/Iris2D/IrisBitmap.h"
+#include "../DirectX/Iris2D/IrisSprite.h"
+#include "../DirectX/Iris2D/IrisRect.h"
+#include "../DirectX/Iris2D/IrisFont.h"
+#include "../DirectX/Iris2D/IrisColor.h"
+#include "../DirectX/Iris2D/IrisViewport.h"
+#endif
+
+#endif
